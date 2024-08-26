810eff1723bf8277f5135930b6e4008a190ac371
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-26 16:51:22 +0600

    event handle

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 const header = document.querySelector("#header");
 const paragraph = document.querySelector("#para");
 
+//
+// const inputs = document.querySelectorAll("input");
+
+
+const inputForms = document.querySelector("#form-group");
+inputForms.addEventListener("change", (e) => {
+    values = e.target.value;
+    if (values === ''){
+        console.log("Values cannot be empty");
+    }
+    alert(values);
+})
+
+
+
+
+
+
+
+
+
+const submitButton = document.querySelector('#subButton')
+submitButton.addEventListener('click', onSubmitHandler)
+
+function onSubmitHandler(e){
+    e.preventDefault()
+    values = .value;
+    if (values === ""){
+        alert("Please enter a info");
+        console.log("please enter a valid info");
+    }
+
+
+}
+
+
 header.style.backgroundColor = "#dddddd";
 header.style.color = "#0909";
 header.style.textAlign = "center";
@@ -11,7 +47,9 @@ header.style.borderRadius = "12px";
 header.textContent = "Hello from JavaScript";
 
 console.log("Hello World");
+//
+// function showAlert() {
+//   alert("This is a function call");
+// }
 
-function showAlert() {
-  alert("This is a function call");
-}
+
