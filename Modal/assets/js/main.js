f64b2a1a15e01a7b7dc3d2ad9bbee39afa77acff
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-29 21:37:21 +0600

    event

--- Modal/assets/js/main.js
+++ Modal/assets/js/main.js
@@ -1,6 +1,8 @@
 const openModal = document.querySelector('#openModal');
 const closeModal = document.querySelector('#closeModal');
 const myModal = document.querySelector('#myModal');
+const nameInput = document.querySelector('#name');
+const inputList = document.querySelectorAll('input-list');
 
 
 openModal.addEventListener('click', (e) => {
@@ -9,3 +11,18 @@ openModal.addEventListener('click', (e) => {
 closeModal.addEventListener('click', (e) => {
     myModal.style.display = 'none';
 })
+window.addEventListener('click', (e) => {
+    if (e.target == myModal) {
+        myModal.style.display = 'none';
+    }
+})
+
+// nameInput.addEventListener('input', (e) => {
+//     values = e.target.value;
+//     inputList.forEach(input => {
+//        const element =  document.createElement('li')
+//         element.textContent = values;
+//
+//     })
+// })
+
