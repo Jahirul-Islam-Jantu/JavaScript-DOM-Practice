490055ec08749b9e752a4939afb6181dd4d1e837
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-30 16:51:37 +0600

    drum kit modified

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,12 +1,13 @@
-window.addEventListener('keydown', (e) => {
+window.addEventListener('keydown', playSounds)
+
+function playSounds(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return // to stop all running
     audio.currentTime = 0; //rewind the start
     audio.play()
     key.classList.add('playing');
-})
-
+}
 
 function removeTransition(e){
     if (e.propertyName !== 'transform') return //skip if it is not a transform
