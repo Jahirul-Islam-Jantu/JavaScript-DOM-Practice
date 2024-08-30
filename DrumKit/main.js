c28512a78ec40bbd124ab8e6331b04961d97493a
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-30 16:48:55 +0600

    drum kit event handle for remove transition

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,3 +1,19 @@
 window.addEventListener('keydown', (e) => {
-    console.log(e)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return // to stop all running
+    audio.currentTime = 0; //rewind the start
+    audio.play()
+    key.classList.add('playing');
+})
+
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return //skip if it is not a transform
+    this.classList.remove('playing')
+}
+
+const key = document.querySelectorAll('.key')
+key.forEach((key) => {
+    key.addEventListener('transitionend', removeTransition)
 })
