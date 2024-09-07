5bb461280237761a877f4c0d1cfc673702ef10e4
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-07 07:54:07 +0600

    change text with dom

---
+++ changeText/main.js
@@ -0,0 +1,8 @@
+const onHandler = () => {
+  let changeText = document.querySelector("#change");
+  changeText.innerText = "On";
+};
+const offHandler = () => {
+  let changeText = document.querySelector("#change");
+  changeText.innerText = "Off";
+};
